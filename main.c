d14d566b69cd7922ed429882e8e6bf7e9f4be739
Kaja W <33781956+mistrzuchna@users.noreply.github.com>
2018-05-05 22:02:35 +0200

    Update main.c
    incomplete algebraic calculating added

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <mem.h>
 #include <math.h>
+#include <stdbool.h>
+
+
+//---------------------------------------------------prototypy----------------------------------------------------------
 
 void ONPInput(char onpPhrase[]);
 
@@ -11,45 +15,63 @@ void pop(struct stack **top, char *result);
 
 char *peek(struct stack *top);
 
-void printToken(char *onpPhrase, struct stack *top);
+void ONPCalculations(char *onpPhrase, struct stack *top);
 
 int menu();
 
+void algebraicExpressionInput(char *algebraicExpression);
+
+void algebraicToONP(char *algebraicExpression, struct stack *top);
+
+char ONPFromAlgebraic[50] = "";
+
+//----------------------------------------------------------------------------------------------------------------------
 
 struct stack {
     char *data;
     struct stack *next;
 };
 
+
+
 int main() {
     struct stack *top = NULL;
     int methodChoice = menu();
     if (methodChoice == 1) {
-        char ONP[20] = "";
+        char ONP[40] = "";
         ONPInput(ONP);
-        printToken(ONP, top);
+        ONPCalculations(ONP, top);
     }
-//
-//    if (menu() == 2) {
-//        //liczenie wyrażeń algebraicznych
-//    }
+
+    if (methodChoice == 2) {
+        char algebraic[50] = "";
+        algebraicExpressionInput(algebraic);
+        algebraicToONP(algebraic, top);
+
+        ONPCalculations(ONPFromAlgebraic, top);
+
+    }
     return 0;
 }
+
+
+//---------------------------------------------------obsługa ONP--------------------------------------------------------
+
 
 void ONPInput(char *onpPhrase) {
     printf("Wprowadz wyrazenie w Odwrotnej Notacji Polskiej.\nOddzielaj poczszegolne liczby i operatory spacjami.\nZakoncz zankiem rownosci");
     puts("");
     puts("");
     scanf(" %[^\n]s", onpPhrase);            // razem ze spacjami
-    printf("Twoje wyrazenie: %s\n", onpPhrase);
+    printf("Twoje wyrazenie: %s\n\n", onpPhrase);
 }
 
-void printToken(char *onpPhrase, struct stack *top) {
+void ONPCalculations(char *onpPhrase, struct stack *top) {
     char *token;
     char s[] = " ";
 
     token = strtok(onpPhrase, s);
-    char *temp = calloc(20, sizeof(char));
+    char *temp = calloc(40, sizeof(char));
     while (token != NULL) {
 
         if (atof(token) != 0) {
@@ -57,7 +79,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             top = push(top, token);
         } else if (*token == '+') {
             // zdejmij 2 elementy ze stosu i dodaj, potem odloz wynik na stos
-            char output[20] = "";
+            char output[40] = "";
             pop(&top, temp);
             double first = atof(temp);
             pop(&top, temp);
@@ -67,7 +89,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             top = push(top, output);
         } else if (*token == '-') {
             // zdejmij 2 elementy ze stosu i dodaj, potem odloz wynik na stos
-            char output[20] = "";
+            char output[40] = "";
             pop(&top, temp);
             double first = atof(temp);
             pop(&top, temp);
@@ -76,7 +98,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             snprintf(output, 50, "%f", result);
             top = push(top, output);
         } else if (*token == '*') {
-            char output[20] = "";
+            char output[40] = "";
             pop(&top, temp);
             double first = atof(temp);
             pop(&top, temp);
@@ -85,7 +107,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             snprintf(output, 50, "%f", result);
             top = push(top, output);
         } else if (*token == '/') {
-            char output[20] = "";
+            char output[40] = "";
             pop(&top, temp);
             double first = atof(temp);
             pop(&top, temp);
@@ -94,7 +116,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             snprintf(output, 50, "%f", result);
             top = push(top, output);
         } else if (*token == '^') {
-            char output[20] = "";
+            char output[40] = "";
             pop(&top, temp);
             double first = atof(temp);
             pop(&top, temp);
@@ -103,7 +125,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             snprintf(output, 50, "%f", result);
             top = push(top, output);
         }else if (*token == '%') {
-            char output[20] = "";
+            char output[40] = "";
             pop(&top, temp);
             double first = atof(temp);
             pop(&top, temp);
@@ -113,7 +135,7 @@ void printToken(char *onpPhrase, struct stack *top) {
             top = push(top, output);
         } else if (*token == '=') {
             if (top->next == NULL)
-                printf("Wynik wprowadzonego wyrazenia wynosi: %.2f", atof(peek(top)));
+                printf("Wynik wprowadzonego wyrazenia wynosi: %.2f\n", atof(peek(top)));
             else
                 printf("Niepoprawny element w wyrazeniu! Sprawdz poprawnosc wpisanych danych.");
         } else
@@ -122,6 +144,12 @@ void printToken(char *onpPhrase, struct stack *top) {
     }
     //printf("\nWierzcholek stosu: \t%s", peek(top));
 }
+
+
+
+//-----------------------------------------------------obsługa stosu----------------------------------------------------
+
+
 
 struct stack *push(struct stack *top, char *data) {
     struct stack *new_node = (struct stack *)
@@ -162,3 +190,68 @@ int menu() {
     scanf("%d", &choice);
     return choice;
 }
+
+
+//-------------------------------------------------obsługa algebraicznego-----------------------------------------------
+
+
+void algebraicExpressionInput(char *algebraicExpression) {
+    printf("Wprowadz wyrazenie w postaci algebraicznej.\nOddzielaj poczszegolne liczby i operatory spacjami.\nZakoncz zankiem rownosci");
+    puts("");
+    puts("");
+    scanf(" %[^\n]s", algebraicExpression);            // razem ze spacjami
+    printf("Twoje wyrazenie: %s\n", algebraicExpression);
+}
+
+void algebraicToONP(char *algebraicExpression, struct stack *top) {
+    //char ONPFromAlgebraic[50] = "";
+    //int currentIndex = -1;
+    //char space[2] = " ";
+
+    char *temp = calloc(40, sizeof(char));
+    char *token;
+    char s[] = " ";
+
+    token = strtok(algebraicExpression, s);
+   // char *temp = calloc(40, sizeof(char));
+    while (token != NULL) {
+
+        if (atof(token) != 0) {
+            // jeśli element jest liczbą odłóż na "wyjście" -- dopisz do ONPFromAlgebraic[]
+            strcat(ONPFromAlgebraic, token);
+            strcat(ONPFromAlgebraic, s);
+
+        } else if (*token == '+') {
+            top = push(top, token);
+        } else if (*token == '-') {
+            top = push(top, token);
+        } else if (*token == '*') {
+            top = push(top, token);
+        } else if (*token == '/') {
+            top = push(top, token);
+        } else if (*token == '^') {
+            top = push(top, token);
+        }else if (*token == '%') {
+            top = push(top, token);
+        } else if (*token == '='){
+            do{
+                pop(&top, temp);
+                strcat(ONPFromAlgebraic, temp);
+                strcat(ONPFromAlgebraic, s);
+            }while(top != NULL);
+            strcat(ONPFromAlgebraic, token);
+        }
+
+
+
+            token = strtok(NULL, s);
+    }
+
+    printf("\nONPFromAlgebraic: %s", ONPFromAlgebraic);
+}
+
+bool priorityCheck(char *operator){ //dalej w programie token po prostu
+    //jeżeli na topie jest ważniejszy, zdjąć wszystkie
+}
+
+//char strcat (char *s, const char *ct) ---- Dopisuje znaki z tablicy znaków ct na koniec tablicy s, zwracane jest s.
